88985be894f577a1fc1cc3af6220dfaf2767ab03
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-04-12 10:48:38 +0200

    project structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AuthView from '@/views/AuthView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import RouterManagmentView from '@/views/RouterManagmentView.vue'
+import UserTrackingView from '@/views/UserTrackingView.vue'
+import AnalyticsView from '@/views/AnalyticsView.vue'
+import SettingsView from '@/views/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/auth',
+      name: 'auth',
+      component: AuthView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/',
+      name: 'dashboard',
+      component: DashboardView,
+    },
+    {
+      path: '/router-managment',
+      name: 'router-managment',
+      component: RouterManagmentView
+    },
+    {
+      path: '/user-tracking',
+      name: 'user-tracking',
+      component: UserTrackingView
+    },
+    {
+      path: '/analytics',
+      name: 'analytics',
+      component: AnalyticsView
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView
     },
   ],
 })
