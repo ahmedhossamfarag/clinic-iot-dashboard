3a9c4b8f936d7188d4091985b36a5dc4ac3f1daa
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-04-15 21:51:39 +0200

    feat: dashboard I

---
+++ src/components/dashboard/BlueprintView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import IconFit from '../shared/icons/IconFit.vue';
+import IconMinus from '../shared/icons/IconMinus.vue';
+import IconPlus from '../shared/icons/IconPlus.vue';
+
+
+</script>
+
+<template>
+    <div class="w-full rounded-xl bg-[#0F172A70] border border-[#1E293B]">
+        <div class="flex p-4 bg-[#1E293B32] border-b border-[#1E293B] gap-2 items-center">
+            <div>
+
+            </div>
+            <div>
+                <p>
+                    Clinic Floor Plan
+                </p>
+            </div>
+            <div>
+                <IconPlus />
+            </div>
+            <div>
+                <IconMinus />
+            </div>
+            <div>
+                <IconFit />
+            </div>
+        </div>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <img />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/dashboard/Card.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: Object,
+        required: true
+    },
+    iconBg: {
+        type: String,
+        required: true
+    },
+    iconColor: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="w-62.5 bg-[#0F172A70] border border-[#1E293B] rounded-xl p-5 pb-8 flex flex-col items-stretch gap-4">
+        <div class="flex justify-between items-center">
+            <div>
+                <p class="text-sm font-medium text-gray-500">{{ title }}</p>
+            </div>
+            <div class="w-8 h-32 flex justify-center items-center rounded-lg" :style="{backgroundColor: iconBg}">
+                <div class="w-5" :style="{color: iconColor}">
+                    <component :is="icon"></component>
+                </div>
+            </div>
+        </div>
+        <div>
+            <p class="text-white text-2xl font-bold">{{ text }}</p>
+        </div>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
