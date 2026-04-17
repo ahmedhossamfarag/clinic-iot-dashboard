642b3669ff32268537c697583bdbfbd316ea6fc4
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-04-17 18:28:48 +0200

    feat: router managment I

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import IconPlus from './icons/IconPlus.vue';
+import IconSearch from './icons/IconSearch.vue';
+
+defineProps({
+    searchPlaceholder: {
+        type: String,
+        default: 'Search ...'
+    },
+    addNewText: {
+        type: String,
+        default: 'Add New'
+    }
+})
+
+const searchQuery = defineModel<string>();
+
+const emit = defineEmits(['add-new'])
+
+const addNew = () => {
+    emit('add-new')
+}
+</script>
+
+<template>
+    <div class="p-4 flex items-center gap-4 border-b border-[#1E293B]">
+        <div class="grow h-9! relative">
+            <div class="w-5 text-[#94A3B8] absolute top-1/2 -translate-y-1/2 left-4">
+                <IconSearch />
+            </div>
+            <input v-model="searchQuery" :placeholder="searchPlaceholder" class="pl-11! h-full!" />
+        </div>
+        <div>
+            <button @click="addNew"
+                class="text-white font-medium text-sm h-full flex items-center gap-2 justify-center bg-[#259DF4] px-4 py-2 rounded-lg">
+                <span class="w-3 text-white">
+                    <IconPlus />
+                </span>
+                {{ addNewText }}
+            </button>
+        </div>
+    </div>
+</template>
--- src/views/RouterManagmentView.vue
+++ src/views/RouterManagmentView.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import Header from '../components/shared/Header.vue';
 import MainLayout from '../components/shared/layout/MainLayout.vue';
+
+const searchQuery = ref('');
 
 </script>
 
 <template>
     <MainLayout>
-        Router Mangament
+        <Header v-model="searchQuery" search-placeholder="Search Users ..." add-new-text="Add User" />
+        <div class="p-8 flex flex-col gap-8 items-stretch">
+            <div class="flex gap-6 justify-between items-center">
+                <div class="flex flex-col g-2" >
+                    <p class="font-black text-3xl text-white">Router Managment</p>
+                    <p class="text-base text-gray-500">Monitor and control your connected IoT infrastructure in real time</p>
+                </div>
+                <div class="py-2 px-4 rounded-2xl bg-[#1E293B80] border border-[#1E293B]">
+                    <p class="text-xs text-gray-500 font-bold">TOTAL ONLINE</p>
+                    <p class="text-xl font-bold text-[#259DF4]">12</p>
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </div>
     </MainLayout>
 </template>
