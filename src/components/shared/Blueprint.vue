8e3caaecbc49a9e7bee317610cb727d90286a26d
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-04-30 23:45:10 +0300

    refactor: dashboard

--- src/components/shared/Blueprint.vue
+++ src/components/shared/Blueprint.vue
@@ -19,8 +19,8 @@ onMounted(() => fetchBlueprint())
         <img class="w-full" :src="blueprintUrl || '/blueprint.png'" />
         <div v-for="item in items" :key="item.id" class="absolute flex gap-3"
             :style="{ top: `${item.location_x * 100}%`, left: `${item.location_y * 100}%` }">
-            <div class="w-2 h-2 rounded-full bg-green-300 outline-8 outline-green-100"></div>
-            <div class="bg-[#334155] rounded-full py-1 px-2 text-white text-base min-w-8 text-center">
+            <div class="w-4 h-4 rounded-full bg-[#27C08D] outline-16 outline-[#27C08D16] outline-offset-8"></div>
+            <div class="bg-[#259DF416] rounded-full py-1 px-2 text-white text-base min-w-8 text-center">
                 {{ item.connected_devices_count }}
             </div>
         </div>
